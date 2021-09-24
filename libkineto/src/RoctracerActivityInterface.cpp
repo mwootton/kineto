a233f2e02f9e4b130576a0e2243d7a96d4626db0
Michael Wootton <michael.wootton@amd.com>
2021-09-24 13:27:59 -0500

    Update api name lookups for rocm4.5 compatibility

--- libkineto/src/RoctracerActivityInterface.cpp
+++ libkineto/src/RoctracerActivityInterface.cpp
@@ -309,20 +309,19 @@ void RoctracerActivityInterface::endTracing() {
 ApiIdList::ApiIdList()
 : m_invert(true)
 {
-  loadApiNames();
 }
 
 void ApiIdList::add(std::string apiName)
 {
-  auto it = m_ids.find(apiName);
-  if (it != m_ids.end()) 
-    m_filter[it->second] = 1;
+  uint32_t cid = 0;
+  if (roctracer_op_code(ACTIVITY_DOMAIN_HIP_API, apiName.c_str(), &cid, NULL) == ROCTRACER_STATUS_SUCCESS)
+    m_filter[cid] = 1;
 }
 void ApiIdList::remove(std::string apiName)
 {
-  auto it = m_ids.find(apiName);
-  if (it != m_ids.end())
-    m_filter.erase(it->second);
+  uint32_t cid = 0;
+  if (roctracer_op_code(ACTIVITY_DOMAIN_HIP_API, apiName.c_str(), &cid, NULL) == ROCTRACER_STATUS_SUCCESS)
+    m_filter.erase(cid);
 }
 
 bool ApiIdList::loadUserPrefs()
@@ -334,14 +333,5 @@ bool ApiIdList::contains(uint32_t apiId)
 {
   return (m_filter.count(apiId) > 0) ? !m_invert : m_invert;  // XOR
 }
-
-void ApiIdList::loadApiNames()
-{
-  // Build lut from apiName to apiId
-  for (uint32_t i = 0; i < HIP_API_ID_NUMBER; ++i) {
-    m_ids[std::string(hip_api_name(i))] = i;
-  }
-}
-
 
 } // namespace KINETO_NAMESPACE
